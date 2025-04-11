bf8568ad43c492c504e7dec21da60a615dcd20b6
unknown <Administrador@ESN501D1278466.educ501.local>
2025-04-11 16:55:22 -0300

    acc

--- teste/exercicio9.py
+++ teste/exercicio9.py
@@ -1,1 +1,25 @@
-print("Hle")
+"""numero = int(input("Digite o numero: "))
+lista = []
+perfeito = 0 
+numerosPerfeitos = []
+
+
+for i in range(1, numero):
+    verificador = numero % i
+    if verificador == 0:
+        lista.append(i)
+print(lista)
+
+for num in lista:
+    perfeito += num
+if perfeito == numero:
+    print("Número perfeito")
+else:
+    print("Número não é perfeito")"""
+
+    
+            
+            
+    
+
+
